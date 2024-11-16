27ab2ab3071e015f4f7867a4e9b14f6988f8fe21
Narendra Kumar Sharma <Sharmanarendra417@gmail.com>
2024-11-16 23:38:39 +0530

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
        <nav className="flex items-center justify-between py-6 ">
         <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-        <img src={logo} className="mx-2 rounded-lg" width={105} height={105} alt="Logo" />
+        <img src={logo} className="mx-2 rounded-lg" width={100} height={100} alt="Logo" />
         </a>
         </div>
         <div>
